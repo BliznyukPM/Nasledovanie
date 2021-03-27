a342f565d4c0e88103fc7f23f086919b058c427c
Nout <pavel.bliznuk@gmail.com>
2021-03-27 04:15:22 +0300

    Создал родительский класс и два дочерних. Так же функцию "монетка" сделанную ранее.

--- main.py
+++ main.py
@@ -12,3 +12,40 @@
 Отправьте одного из солдат первого героя следовать за ним. Выведите на экран идентификационные номера
 этих двух юнитов.
 '''
+import random
+import uuid
+
+class Unit:
+    # parent class Unit
+    def __init__(self,id,team):
+        self.id=id
+        self.team=team
+class Hero(Unit):
+    #class Hero
+    def __init__(self,id,team,lvl=1):
+        Unit.__init__(self,id,team)
+        self.lvl=lvl
+    def lvl_up(self):
+        #method of increasing the level
+        self.lvl+=1
+class Soldier(Unit):
+    #class Soldier
+    def __init__(self,id,team,gofh=False):
+        Unit.__init__(self,id,team)
+        self.gofh=gofh
+    def goin_fo_the_hero(self):
+        #method going fo the hero
+        self.gofh+=1
+
+def coin():
+    #function coin for random
+    b = random.randint(1,4)
+    print(b)
+    if b % 2 == 0:
+        return (True)
+    else:
+        return (False)
+
+generalNOD=Hero(int(uuid.uuid4()),'NOD')
+generalGDI=Hero(int(uuid.uuid4()),'GDI')
+
