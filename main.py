94529b0925815e83bd3770876b233d5dac43fa2c
Nout <pavel.bliznuk@gmail.com>
2021-03-31 01:39:57 +0300

    пытаюсь сделать команды объектомм

--- main.py
+++ main.py
@@ -22,13 +22,16 @@ class Unit:
         self.team=team
 class Hero(Unit):
     #class Hero
-    def __init__(self,name_hero,id,team,lvl=1):
+    def __init__(self,id,team,lvl=1):
         Unit.__init__(self,id,team)
         self.lvl=lvl
-        self.name_hero=name_hero
+        self.team=team
+        n=Teams(team)
+        teams_list.append(n)
     def lvl_up(self):
         #method of increasing the level
         self.lvl+=1
+
 class Soldier(Unit):
     #class Soldier
     def __init__(self,id,team,gofh=False):
@@ -37,7 +40,16 @@ class Soldier(Unit):
     def goin_to_the_hero(self):
         #method going fo the hero
         self.gofh=True
+class Teams:
+    comand = []
+    def __init__(self,team_name):
+        self.team_name=team_name
 
+    def commands(self,s):
+        self.s=s
+        Teams.comand=comand.append(s)
+    def prints(self):
+        print(comand())
 
 def belonging_to_the_teem():
     #belonging to the team GDI or NOD
@@ -55,29 +67,32 @@ def general_lvl_up():
     else:
         generalGDI.lvl_up()
         generalNOD.lvl_up()
-
+teams_list=[]
 generalGDI=Hero(int(uuid.uuid4()),'GDI')
 generalNOD=Hero(int(uuid.uuid4()),'NOD')
-
+print(teams_list)
+teams_list[0].prints
 number_of_soldiers=100
 list_NOD=[]             #list team Nod
 list_GDI=[]             #list team GDI
-
+''''''
 for k in range(number_of_soldiers):
-    #distribute soldiers to team NOD or GDI
+    #generate soldiers and distribute to team NOD or GDI
     s=Soldier(int(uuid.uuid4()),belonging_to_the_teem())
-    if s.team=='NOD':
-        list_NOD.append(s)
+    if s.team==teams_list[0].team_name:
+        teams_list[0].commands(s)
     else:
-        list_GDI.append(s)
+        teams_list[1].commands(s)
+teams_list[0].prints
+''''''
+#length_NOD=len(teams_list[1])
+#length_GDI=len(teams_list[0])
+#print('команда GDI ',length_GDI,' солдат')
+#print('команда NOD ',length_NOD,' солдат')
 
-length_NOD=len(list_NOD)
-length_GDI=len(list_GDI)
-print('команда GDI ',length_GDI,' солдат')
-print('команда NOD ',length_NOD,' солдат')
-
-general_lvl_up()
-
+#general_lvl_up()
+'''
 list_GDI[0].goin_to_the_hero()
 print('id солдата следующего за генералом ',list_GDI[0].id)
 print('id генерала за которым следует солдат ',generalGDI.id)
+'''
